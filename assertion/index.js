2a86ca17c2bbb67b4a62be8e85e933b9bd76881e
Nathan Queija <nathanqcam@gmail.com>
2019-06-05 17:29:19 +0200

    add test helper to encapsulate try catch

--- assertion/index.js
+++ assertion/index.js
@@ -1,8 +1,18 @@
-const expect = actual => ({
+export const expect = actual => ({
   toBe: expected => {
     if (actual !== expected)
       throw new Error(`${actual} is not equal to ${expected}`);
-  }
+  },
+  toEqual: () => {},
+  toBeGreaterThan: expected => actual > expected
 });
 
-export default expect;
+export const test = (title, callback) => {
+  try {
+    callback();
+    console.log(`✅ ${title}`);
+  } catch (error) {
+    console.error(`❌ ${title}`);
+    console.error(error);
+  }
+};
